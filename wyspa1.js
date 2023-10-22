70b5bac21d6280061e51b2ac3edff2594f9d17ef
kubaadamczak12 <91568135+kubaadamczak12@users.noreply.github.com>
2023-10-22 21:21:41 +0200

    Add files via upload

---
+++ wyspa1.js
@@ -0,0 +1,121 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+var window_height = window.innerHeight; 
+var window_width = window.innerWidth;
+canvas.width = window_width;
+canvas.height = window_height;
+
+
+context.beginPath();
+context.arc(900,500, 330, 0,Math.PI * 2, false);// wyspa jebana 
+context.stroke();
+context.closePath();
+
+
+context.beginPath();
+context.strokeRect(1000, 600, 160, 70); //skrzynia 1
+context.strokeRect(1030, 550, 130, 50); //skrzynia 
+
+
+
+
+
+context.strokeRect(1000, 400, 39, 111);//drzewo 1
+context.strokeRect(670, 400, 39, 50)//liscie
+context.strokeRect(670, 400, 39, 50)//liscei
+context.strokeRect(730, 400, 39, 50)
+context.arc(755,450, 10, 0,Math.PI * 2, false)
+context.strokeRect(700, 370, 39, 30)
+context.strokeRect(670, 400, 39, 50)
+context.arc(690,450, 10, 0,Math.PI * 2, false)
+
+
+context.strokeRect(700, 400, 39, 111); //drzewo 2
+context.strokeRect(970, 400, 39, 50)//liscie
+context.strokeRect(1030, 400, 39, 50)//liscie
+context.strokeRect(1000, 370, 39, 50)//liscie
+
+
+context.strokeRect(960, 450, 30, 10); //kokos
+context.strokeRect(1050, 450, 30, 10); //kokos2
+
+
+context.stroke();
+context.closePath();
+
+
+
+
+context.stroke();
+context.closePath();
+
+
+
+context.beginPath();
+context.arc(400,230, 33, 0,Math.PI * 2, false);// slonce
+context.stroke();
+context.closePath();
+
+
+context.beginPath();
+context.arc(1050,597, 6, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1080,597, 7, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1100,597, 3, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1130,597, 4, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1150,597, 6, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1110,597, 5, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1090,597, 4, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1040,597, 5, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1060,597, 6, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1070,597, 4, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1140,597, 7, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(1120,597, 5, 0,Math.PI * 2, false);// element od skrzyni
+context.stroke();
+context.closePath();
+
+
+
